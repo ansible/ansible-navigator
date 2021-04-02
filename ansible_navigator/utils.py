911d1251393c27b2bc3c8aa0775d0ba5bfdb1e76
Rick Elrod <rick@elrod.me>
2021-04-02 17:59:57 -0500

    black
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- ansible_navigator/utils.py
+++ ansible_navigator/utils.py
@@ -217,14 +217,17 @@ def get_conf_dir(filename: Optional[str] = None) -> Tuple[Optional[str], List[st
         must_exist = os.path.join(path, filename) if filename is not None else path
         if not os.path.exists(must_exist):
             msgs.append(
-                "Skipping {0} because required file {1} does not exist".format(path, filename))
+                "Skipping {0} because required file {1} does not exist".format(path, filename)
+            )
             continue
 
         try:
             perms = os.stat(path)
             if perms.st_mode & stat.S_IWOTH:
                 msgs.append(
-                    "Ignoring potential configuration directory {0} because it is world-writable.".format(path)
+                    "Ignoring potential configuration directory {0} because it is world-writable.".format(
+                        path
+                    )
                 )
                 continue
             return (path, msgs)
