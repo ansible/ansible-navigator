e9cf4a5d39f849d95f08c076cc0fef36b9ef1e0a
Bradley Thornton <bthornto@redhat.com>
2021-04-08 08:10:24 -0700

    Remove unecessary log statement

--- ansible_navigator/ui_framework/ui.py
+++ ansible_navigator/ui_framework/ui.py
@@ -837,5 +837,4 @@ class UserInterface(CursesWindow):
             result = self._show_menu(obj, columns, await_input)
         else:
             result = self._show_obj_from_list([obj], 0, await_input)
-        self._logger.debug(result)
         return result
