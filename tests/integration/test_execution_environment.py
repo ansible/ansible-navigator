4eeeca23f7eeadafce408518e80611e62d3c60ea
Bradley Thornton <bthornto@redhat.com>
2021-04-19 09:07:46 -0700

    add debug for envvars

--- tests/integration/test_execution_environment.py
+++ tests/integration/test_execution_environment.py
@@ -67,4 +67,4 @@ class Test(Cli2Runner):
             assert "process_isolation" not in kwargs
         else:
             for item in expected.items():
-                assert False #item in kwargs.items()
+                assert item in kwargs.items()
