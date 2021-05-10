c1897bdfce9b01e90a8b8b99e364c89a6b687b32
Ganesh Nalawade <ganesh634@gmail.com>
2021-05-10 07:04:18 +0530

    Refactor collections action to use new runner API (#211)
    Refactor collections action to use new runner API
    
    Reviewed-by: https://github.com/apps/ansible-zuul

---
+++ tests/integration/actions/collections/base.py
@@ -0,0 +1,79 @@
+""" base class for config interactive tests
+"""
+import difflib
+import json
+import os
+import shutil
+
+import pytest
+
+from ..._common import fixture_path_from_request
+from ..._common import update_fixtures
+from ..._common import TmuxSession
+
+from ....defaults import FIXTURES_COLLECTION_DIR
+
+
+class BaseClass:
+    # pylint: disable=attribute-defined-outside-init
+    """base class for interactive collections tests"""
+
+    UPDATE_FIXTURES = False
+    EXECUTION_ENVIRONMENT_TEST = False
+
+    @staticmethod
+    @pytest.fixture(scope="module", name="tmux_collections_session")
+    def _fixture_tmux_config_session(request, os_indendent_tmp):
+        """tmux fixture for this module"""
+
+        tmp_coll_dir = os.path.join(os_indendent_tmp, request.node.name, "")
+        try:
+            shutil.rmtree(tmp_coll_dir)
+        except FileNotFoundError:
+            pass
+        os.makedirs(tmp_coll_dir)
+        shutil.copytree(FIXTURES_COLLECTION_DIR, os.path.join(tmp_coll_dir, "collections"))
+        params = {
+            "window_name": request.node.name,
+            "setup_commands": [
+                f"cd {tmp_coll_dir}",
+                f"export ANSIBLE_COLLECTIONS_PATH={tmp_coll_dir}",
+                "export ANSIBLE_DEVEL_WARNING=False",
+                "export ANSIBLE_DEPRECATION_WARNINGS=False",
+            ],
+            "pane_height": "2000",
+            "pane_width": "200",
+        }
+        with TmuxSession(**params) as tmux_session:
+            yield tmux_session
+
+    def test(
+        self,
+        request,
+        os_indendent_tmp,
+        tmux_collections_session,
+        index,
+        user_input,
+        comment,
+        collection_fetch_prompt,
+    ):
+        # pylint:disable=unused-argument
+        # pylint: disable=too-few-public-methods
+        # pylint: disable=too-many-arguments
+        """test interactive config"""
+        received_output = tmux_collections_session.interaction(
+            user_input, wait_on_collection_fetch_prompt=collection_fetch_prompt
+        )
+
+        received_output = [
+            line.replace(os_indendent_tmp, "FIXTURES_COLLECTION_DIR") for line in received_output
+        ]
+
+        if self.UPDATE_FIXTURES:
+            update_fixtures(request, index, received_output, comment)
+        dir_path, file_name = fixture_path_from_request(request, index)
+        with open(f"{dir_path}/{file_name}") as infile:
+            expected_output = json.load(infile)["output"]
+        assert expected_output == received_output, "\n" + "\n".join(
+            difflib.unified_diff(expected_output, received_output, "expected", "received")
+        )
---
+++ tests/integration/actions/collections/test_welcome_interactive_noee.py
@@ -0,0 +1,37 @@
+""" collections from welcome interactive w/0 ee
+"""
+import pytest
+
+from .base import BaseClass
+
+
+CLI = "ansible-navigator --execution-environment false"
+
+testdata = [
+    (0, CLI, "ansible-navigator welcome screen", None),
+    (
+        1,
+        ":collections",
+        "ansible-navigator collections top window",
+        "Collecting collection content",
+    ),
+    (2, ":0", "Browse testorg.coll_1 plugins window", None),
+    (3, ":0", "lookup_1 plugin docs window", None),
+    (4, ":back", "Back to browse testorg.coll_1 plugins window", None),
+    (5, ":1", "mod_1 plugin docs window", None),
+    (6, ":back", "Back to browse testorg.coll_1 plugins window", None),
+    (7, ":back", "Back to ansible-navigator collections browse window", None),
+    (8, ":1", "Browse testorg.coll_2 plugins window", None),
+    (9, ":0", "lookup_2 plugin docs window", None),
+    (10, ":back", "Back to browse testorg.coll_2 plugins window", None),
+    (11, ":1", "mod_2 plugin docs window", None),
+    (12, ":back", "Back to browse testorg.coll_2 plugins window", None),
+    (13, ":back", "Back to ansible-navigator collections browse window", None),
+]
+
+
+@pytest.mark.parametrize("index, user_input, comment, collection_fetch_prompt", testdata)
+class Test(BaseClass):
+    """run the tests"""
+
+    UPDATE_FIXTURES = False
