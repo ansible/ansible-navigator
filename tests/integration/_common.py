d922116ded1bdee9e205cf74913dd4c7a27f895c
Bradley Thornton <bthornto@redhat.com>
2021-05-13 04:55:33 -0700

    copy log

--- tests/integration/_common.py
+++ tests/integration/_common.py
@@ -4,6 +4,8 @@ import datetime
 import os
 import re
 import shlex
+import shutil
+import subprocess
 import sys
 import tempfile
 import time
@@ -261,6 +263,9 @@ class TmuxSession:
                     message += self._setup_screen
                     message += ["*********************** showing *****************************"]
                     message += showing
+                    message.append(subprocess.check_output("ls ~", shell=True).decode("utf-8"))
+                    message.append(subprocess.check_output("cp ansible-navigator.log ~/zuul-output/", shell=True).decode("utf-8"))
+                    message.append(subprocess.check_output("cat ansible-navigator.log", shell=True).decode("utf-8"))
                     return message
             if wait_on_cli_prompt:
                 # handle command sent but pane not updated
