98148bd94505daa70bde328c1eac96a0249c4a5a
Bradley Thornton <bthornto@redhat.com>
2021-05-14 08:03:19 -0700

    Black on setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,23 @@
 """ setup """
+import glob
+import os
+from itertools import groupby
+from operator import itemgetter
+
 from setuptools import setup
 
-setup()
+# valid files in share to be copied
+data_file_types = [".yml", ".py", ".json", ".md"]
+
+# tuples of (dir, file)
+file_pairs = [
+    (x[0], y)
+    for x in os.walk("share")
+    for y in glob.glob(os.path.join(x[0], "**"))
+    if os.path.isfile(y) and os.path.splitext(y)[1] in data_file_types
+]
+
+# tuples of (dir, [files])
+data_file_list = [(k, list(list(zip(*g))[1])) for k, g in groupby(file_pairs, itemgetter(0))]
+
+setup(data_files=data_file_list)
