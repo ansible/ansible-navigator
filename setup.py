b2d66ff5dc917b25d56897d1afcdae8e7bafcf83
Bradley Thornton <bthornto@redhat.com>
2021-05-14 12:19:17 -0700

    CPU count 50%, improve desc in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 # valid files in share to be copied
 data_file_types = [".yml", ".py", ".json", ".md"]
 
-# tuples of (dir, file)
+# generate a [(dir, file),...] for all share files
 file_pairs = [
     (x[0], y)
     for x in os.walk("share")
@@ -17,7 +17,14 @@ file_pairs = [
     if os.path.isfile(y) and os.path.splitext(y)[1] in data_file_types
 ]
 
-# tuples of (dir, [files])
+# group by dir [(dir, [files, file]),...]
+# data_files specifies a sequence of (directory, files) pairs in the following way:
+# setup(...,
+#     data_files=[('bitmaps', ['bm/b1.gif', 'bm/b2.gif']),
+#                 ('config', ['cfg/data.cfg'])],
+#     )
+# Each (directory, files) pair in the sequence specifies the installation directory
+# and the files to install there.
 data_file_list = [(k, list(list(zip(*g))[1])) for k, g in groupby(file_pairs, itemgetter(0))]
 
 setup(data_files=data_file_list)
