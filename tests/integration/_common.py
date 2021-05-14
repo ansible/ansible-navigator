ea48ab00e4b6490c1acb58d886f8cc479496967e
Bradley Thornton <bthornto@redhat.com>
2021-05-14 13:59:36 -0700

    copytree replacement

--- tests/integration/_common.py
+++ tests/integration/_common.py
@@ -1,6 +1,8 @@
 """ some common funcs for the tests
 """
 import os
+import shutil
+import stat
 import sys
 import json
 
@@ -71,3 +73,35 @@ def generate_test_log_dir(unique_test_id):
         directory = os.path.join("./", ".test_logs", unique_test_id)
     os.makedirs(directory, exist_ok=True)
     return directory
+
+
+def copytree(src, dst, symlinks=False, ignore=None):
+    """Copy a dir without failing if it exists
+    distutils is beign deprecated
+    shutil.copytree exist in 3.8 only
+    https://stackoverflow.com/questions/1868714
+    """
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+        shutil.copystat(src, dst)
+    lst = os.listdir(src)
+    if ignore:
+        excl = ignore(src, lst)
+        lst = [x for x in lst if x not in excl]
+    for item in lst:
+        src = os.path.join(src, item)
+        dst = os.path.join(dst, item)
+        if symlinks and os.path.islink(s):
+            if os.path.lexists(dst):
+                os.remove(dst)
+            os.symlink(os.readlink(src), dst)
+            try:
+                stt = os.lstat(src)
+                mode = stat.S_IMODE(stt.st_mode)
+                os.lchmod(dst, mode)
+            except Exception:  # pylint: disable=broad-except
+                pass  # lchmod not available
+        elif os.path.isdir(src):
+            copytree(src, dst, symlinks, ignore)
+        else:
+            shutil.copy2(src, dst)
