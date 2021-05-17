ee10207c583d1406b597904b003d3e5423f991d3
Bradley Thornton <bthornto@redhat.com>
2021-05-17 07:53:16 -0700

    Add param support for :config

--- tests/integration/actions/config/base.py
+++ tests/integration/actions/config/base.py
@@ -2,23 +2,95 @@
 """
 import difflib
 import json
+import logging
+import shlex
+
+from enum import Enum
+from typing import List
+from typing import NamedTuple
+from typing import Union
+
 import pytest
 
-from typing import Optional
+from ..._common import container_runtime_or_fail
 from ..._common import fixture_path_from_request
 from ..._common import update_fixtures
 from ..._tmux_session import TmuxSession
+
+
+class SearchFor(Enum):
+    """set the test mode"""
+
+    HELP = "search for help"
+
+
+class Command(NamedTuple):
+    """command details"""
+
+    execution_environment: bool
+    container_engine: str = container_runtime_or_fail()
+    command: str = "ansible-navigator"
+    log_level: str = "debug"
+    mode: str = "interactive"
+    subcommand: Union[None, str] = None
+
+    def join(self):
+        """create cli command"""
+        args = []
+        args.append(self.command)
+        if self.subcommand is not None:
+            args.append(self.subcommand)
+        args.extend(["--ee", self.execution_environment])
+        args.extend(["--ce", self.container_engine])
+        args.extend(["--ll", self.log_level])
+        args.extend(["--mode", self.mode])
+        return " ".join(shlex.quote(str(arg)) for arg in args)
+
+
+class Step(NamedTuple):
+    # pylint: disable=too-few-public-methods
+    """test data object"""
+    user_input: str
+    comment: str
+
+    look_fors: List[str] = []
+    look_nots: List[str] = []
+    mask: bool = True
+    playbook_status: Union[None, str] = None
+    step_index: int = 0
+    search_within_response: Union[SearchFor, str] = SearchFor.HELP
+
+
+def add_indicies(steps):
+    """update the index of each"""
+    return (step._replace(step_index=idx) for idx, step in enumerate(steps))
+
+
+def step_id(value):
+    """return the test id from the test step object"""
+    return f"{value.step_index}-{value.user_input}-{value.comment}"
+
+
+base_steps = (
+    Step(user_input=":f CACHE_PLUGIN_TIMEOUT", comment="filter for cache plugin timeout"),
+    Step(user_input=":0", comment="cache plugin details"),
+    Step(user_input=":back", comment="return to filtered list"),
+    Step(user_input=":f", comment="clear filter, full list"),
+    Step(user_input=":f yaml", comment="filter off screen value"),
+    Step(user_input=":3", comment="YAML_FILENAME_EXTENSIONS details"),
+    Step(user_input=":back", comment="return to filtered list"),
+    Step(user_input=":f", comment="clear filter, full list"),
+)
 
 
 class BaseClass:
     """base class for interactive/stdout config tests"""
 
     UPDATE_FIXTURES = False
-    TEST_FOR_MODE: Optional[str] = None
 
     @staticmethod
-    @pytest.fixture(scope="module", name="tmux_config_session")
-    def fixture_tmux_config_session(request):
+    @pytest.fixture(scope="module", name="tmux_session")
+    def fixture_tmux_session(request):
         """tmux fixture for this module"""
         params = {
             "setup_commands": ["export ANSIBLE_CACHE_PLUGIN_TIMEOUT=42"],
@@ -27,46 +99,54 @@ class BaseClass:
         with TmuxSession(**params) as tmux_session:
             yield tmux_session
 
-    def test(
-        self, request, tmux_config_session, index, user_input, comment, testname, expected_in_output
-    ):
+    def test(self, request, tmux_session, step):
         # pylint:disable=unused-argument
         # pylint: disable=too-few-public-methods
         # pylint: disable=too-many-arguments
         """test interactive/stdout config"""
 
-        if self.TEST_FOR_MODE == "interactive":
+        if step.search_within_response is SearchFor.HELP:
             search_within_response = ":help help"
-        elif self.TEST_FOR_MODE == "stdout":
-            search_within_response = tmux_config_session.cli_prompt
         else:
-            raise ValueError(
-                "Value of 'TEST_FOR_MODE' is not set."
-                " Valid value is either 'interactive' or 'stdout'"
+            raise ValueError("test mode not set")
+
+        received_output = tmux_session.interaction(
+            value=step.user_input,
+            search_within_response=search_within_response,
+        )
+
+        if step.mask:
+            # mask out some config that is subject to change each run
+            mask = "X" * 50
+            for idx, line in enumerate(received_output):
+                if "13│BECOME_PLUGIN_PATH" in line:
+                    received_output[idx] = mask
+                if "15│CACHE_PLUGIN_CONNECTION" in line:
+                    received_output[idx] = mask
+
+        if self.UPDATE_FIXTURES:
+            update_fixtures(
+                request,
+                step.step_index,
+                received_output,
+                step.comment,
+                additional_information={
+                    "look_fors": step.look_fors,
+                    "look_nots": step.look_nots,
+                    "compared_fixture": not any((step.look_fors, step.look_nots)),
+                },
             )
 
-        received_output = tmux_config_session.interaction(user_input, search_within_response)
-
-        if expected_in_output:
-            for out in expected_in_output:
-                assert any(out in line for line in received_output), (out, received_output)
+        page = " ".join(received_output)
+        if step.look_fors:
+            assert all(look_for in page for look_for in step.look_fors)
+        elif step.look_nots:
+            assert not any(look_not in page for look_not in step.look_nots)
         else:
-            # mask out some config that is subject to change each run
-            for idx, line in enumerate(received_output):
-                mask = "X" * 50
-                if tmux_config_session.cli_prompt in line:
-                    received_output[idx] = mask
-                else:
-                    if "13│BECOME_PLUGIN_PATH" in line:
-                        received_output[idx] = mask
-                    if "15│CACHE_PLUGIN_CONNECTION" in line:
-                        received_output[idx] = mask
-
-            if self.UPDATE_FIXTURES:
-                update_fixtures(request, index, received_output, comment, testname=testname)
-            dir_path, file_name = fixture_path_from_request(request, index, testname=testname)
+            dir_path, file_name = fixture_path_from_request(request, step.step_index)
             with open(f"{dir_path}/{file_name}") as infile:
                 expected_output = json.load(infile)["output"]
+
             assert expected_output == received_output, "\n" + "\n".join(
                 difflib.unified_diff(expected_output, received_output, "expected", "received")
             )
--- tests/integration/actions/config/test_welcome_interactive_param_use.py
+++ tests/integration/actions/config/test_welcome_interactive_param_use.py
@@ -12,12 +12,24 @@ from .base import Step
 
 CLI = Command(execution_environment=False).join()
 
-initial_steps = (
+steps = (
     Step(user_input=CLI, comment="welcome screen"),
-    Step(user_input=":config", comment="enter config from welcome screen"),
+    Step(
+        user_input=":config",
+        comment="enter config from welcome screen",
+        mask=False,
+        look_nots=["/home/runner"],
+    ),
+    Step(user_input=":back", comment="return to welcome screen"),
+    Step(
+        user_input=":config --ee True",
+        comment="enter config from welcome screen",
+        mask=False,
+        look_fors=["/home/runner"],
+    ),
 )
 
-steps = add_indicies(initial_steps + base_steps)
+steps = add_indicies(steps)
 
 
 @pytest.mark.parametrize("step", steps, ids=step_id)
