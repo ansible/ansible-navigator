749f0f86f96d27e8ef92bb218ac278f8814723d2
Bradley Thornton <bthornto@redhat.com>
2021-05-17 19:36:07 -0700

    120

--- tests/integration/_tmux_session.py
+++ tests/integration/_tmux_session.py
@@ -156,7 +156,7 @@ class TmuxSession:
         value,
         search_within_response: Union[None, List, str] = None,
         ignore_within_response=None,
-        timeout=60,
+        timeout=120,
     ):
         """interact with the tmux session
         :param value: send to screen
