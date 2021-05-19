5965534f88be955dca5eec3dec4afe7d7faaca78
Bradley Thornton <bthornto@redhat.com>
2021-05-19 11:34:01 -0700

    Lorem ipsum line length

--- ansible_navigator/actions/sample_notification.py
+++ ansible_navigator/actions/sample_notification.py
@@ -17,8 +17,8 @@ form:
       information:
       - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
       - labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-      - laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      - velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+      - laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+      - velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non,
       - sunt in culpa qui officia deserunt mollit anim id est laborum.
       type: information
   type: notification
