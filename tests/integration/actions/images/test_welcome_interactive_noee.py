a0cde1fe853f5590280743911594eaf56a9ac787
Bradley Thornton <bthornto@redhat.com>
2021-05-26 12:19:14 -0700

    images tests

---
+++ tests/integration/actions/images/test_welcome_interactive_noee.py
@@ -0,0 +1,31 @@
+""" welcome interactive w/o ee
+"""
+import pytest
+
+from .base import base_steps
+from .base import BaseClass
+from .base import IMAGE_SHORT
+
+from ..._interactions import add_indicies
+from ..._interactions import Command
+from ..._interactions import Step
+from ..._interactions import step_id
+
+# this is misleadeing b/c images will use an ee, but not for automation
+CLI = Command(execution_environment=False).join()
+
+initial_steps = (
+    Step(user_input=CLI, comment="welcome screen"),
+    Step(
+        user_input=":images", comment="ansible-navigator images top window", look_fors=[IMAGE_SHORT]
+    ),
+)
+
+steps = add_indicies(initial_steps + base_steps)
+
+
+@pytest.mark.parametrize("step", steps, ids=step_id)
+class Test(BaseClass):
+    """run the tests"""
+
+    UPDATE_FIXTURES = True
