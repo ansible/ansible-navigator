aec0ec75d1248ec0868f374c7abc8eab9c809b38
cidrblock <brad@thethorntons.net>
2021-05-29 10:10:03 -0700

    test fix, add no color test for colorize

--- tests/unit/ui_framework/test_colorize.py
+++ tests/unit/ui_framework/test_colorize.py
@@ -12,13 +12,17 @@ from ansible_navigator.yaml import human_dump
 SHARE_DIR = os.path.abspath(
     os.path.join(os.path.basename(__file__), "..", "share", "ansible_navigator")
 )
+THEME_PATH = os.path.join(SHARE_DIR, "themes", "dark_vs.json")
+GRAMMAR_DIR = os.path.join(SHARE_DIR, "grammar")
 
 
 def test_basic_success_json():
     """Ensure the json string is returned as 1 lines, 5 parts and can be reassembled
     to the json string"""
     sample = json.dumps({"test": "data"})
-    result = Colorize(SHARE_DIR).render(doc=sample, scope="source.json")
+    result = Colorize(grammar_dir=GRAMMAR_DIR, theme_path=THEME_PATH).render(
+        doc=sample, scope="source.json"
+    )
     assert len(result) == 1
     assert len(result[0]) == 5
     assert "".join(line_part["chars"] for line_part in result[0]) == sample
@@ -30,12 +34,27 @@ def test_basic_success_yaml():
     the second line of the yaml string
     """
     sample = human_dump({"test": "data"})
-    result = Colorize(SHARE_DIR).render(doc=sample, scope="source.yaml")
+    result = Colorize(grammar_dir=GRAMMAR_DIR, theme_path=THEME_PATH).render(
+        doc=sample, scope="source.yaml"
+    )
     assert len(result) == 2
     assert len(result[0]) == 1
     assert result[0][0]["chars"] == sample.splitlines()[0]
     assert len(result[1]) == 3
     assert "".join(line_part["chars"] for line_part in result[1]) == sample.splitlines()[1]
+
+
+def test_basic_success_no_color():
+    """Ensure scope nocolor return just lines"""
+    sample = json.dumps({"test": "data"})
+    result = Colorize(grammar_dir=GRAMMAR_DIR, theme_path=THEME_PATH).render(
+        doc=sample, scope="no_color"
+    )
+    assert len(result) == 1
+    assert len(result[0]) == 1
+    assert result[0][0]["chars"] == sample
+    assert result[0][0]["color"] is None
+    assert result[0][0]["column"] == 0
 
 
 @patch("ansible_navigator.ui_framework.colorize.tokenize")
@@ -45,7 +64,9 @@ def test_graceful_failure(mocked_func, caplog):
     """
     mocked_func.side_effect = ValueError()
     sample = json.dumps({"test": "data"})
-    result = Colorize(SHARE_DIR).render(doc=sample, scope="source.json")
+    result = Colorize(grammar_dir=GRAMMAR_DIR, theme_path=THEME_PATH).render(
+        doc=sample, scope="source.json"
+    )
     assert len(result) == 1
     assert len(result[0]) == 1
     assert result[0][0]["chars"] == sample
