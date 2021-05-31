9563043c4ccd3d17d2888b43464ceaa0340c12be
Bradley A. Thornton <bthornto@redhat.com>
2021-05-31 11:28:35 -0700

    [ui] Templar improvements and bug fixes (#399)
    [ui] Templar improvements and bug fixes
    
    Reviewed-by: https://github.com/apps/ansible-zuul

---
+++ tests/integration/actions/templar/test_direct_interactive_ee.py
@@ -0,0 +1,35 @@
+""" run direct from cli interactive with ee
+"""
+import pytest
+
+from .base import BaseClass
+from .base import base_steps
+from .base import inventory_path
+from .base import playbook_path
+
+
+from ..._interactions import add_indicies
+from ..._interactions import Command
+from ..._interactions import Step
+from ..._interactions import step_id
+
+
+cmdline = f"{playbook_path} -i {inventory_path}"
+CLI = Command(subcommand="run", cmdline=cmdline, execution_environment=True).join()
+
+initial_steps = (
+    Step(
+        user_input=CLI,
+        comment="ansible-navigator run playbook",
+        search_within_response=["COMPLETE", "SUCCESSFUL"],
+    ),
+)
+
+steps = add_indicies(initial_steps + base_steps)
+
+
+@pytest.mark.parametrize("step", steps, ids=step_id)
+class Test(BaseClass):
+    """run the tests"""
+
+    UPDATE_FIXTURES = False
---
+++ tests/integration/actions/templar/test_welcome_interactive_ee.py
@@ -0,0 +1,36 @@
+""" from welcome interactive with ee
+"""
+import pytest
+
+from .base import BaseClass
+from .base import base_steps
+from .base import inventory_path
+from .base import playbook_path
+
+
+from ..._interactions import add_indicies
+from ..._interactions import Command
+from ..._interactions import Step
+from ..._interactions import step_id
+
+
+CLI = Command(execution_environment=True).join()
+cmdline = f":run {playbook_path} -i {inventory_path}"
+
+initial_steps = (
+    Step(user_input=CLI, comment="welcome screen"),
+    Step(
+        user_input=cmdline,
+        comment="ansible-navigator run playbook",
+        search_within_response=["COMPLETE", "SUCCESSFUL"],
+    ),
+)
+
+steps = add_indicies(initial_steps + base_steps)
+
+
+@pytest.mark.parametrize("step", steps, ids=step_id)
+class Test(BaseClass):
+    """run the tests"""
+
+    UPDATE_FIXTURES = False
