17fc8565693e09335ae2e8eca232e6a99f7b7f02
cidrblock <brad@thethorntons.net>
2021-06-14 06:53:35 -0700

    XXXX

--- ansible_navigator/ui_framework/curses_window.py
+++ ansible_navigator/ui_framework/curses_window.py
@@ -211,9 +211,12 @@ class CursesWindow:
         """
         if paint_blanks:
             for line in range(self._screen_h):
-                c_line = (CursesLinePart(0, " " * self._screen_w, 0, 0),)
+                c_line = (CursesLinePart(0, "X" * self._screen_w, 0, 0),)
                 self._add_line(self._screen, line, c_line)
             self._screen.refresh()
+        
+        import time
+        time.sleep(5)
 
         self._screen.clear()
         self._screen.refresh()
