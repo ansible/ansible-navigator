5fd93e90b6ba39e0c7c66048e565fad1dfaa6b0d
Rick Elrod <rick@elrod.me>
2021-10-11 13:35:03 -0500

    [runner.api] Let command runner wrap with sh -c
    Change:
    - In some cases, when using the runner API to call into external
      commands, we might wish to retrieve *only* stdout. With runner
      defaults (pexpect), it will force --tty, which will munge the
      containerized command's stdout and stderr all into just stdout.
      (see: https://git.io/JoAAR and https://git.io/JoAxi)
    - With this change, we can wrap the containerized command in
      `/bin/sh -c` by passing an argument to the constructor of
      CommandBaseRunner and ignore stderr output.
    
    Test Plan:
    - Tested with new, not-yet-pushed :lint action
    - New units
    
    Signed-off-by: Rick Elrod <rick@elrod.me>

---
+++ tests/unit/runner/test_api.py
@@ -0,0 +1,53 @@
+""" tests for runner.api
+"""
+
+import pytest
+
+from ansible_navigator.runner import api
+
+
+@pytest.mark.parametrize(
+    "orig_exec, orig_args, sh_c_arg",
+    [
+        (
+            "ansible-lint",
+            [],
+            "ansible-lint 2>/dev/null",
+        ),
+        (
+            "ansible-lint",
+            ["/tmp/myroles/supercool/tasks/main.yml"],
+            "ansible-lint /tmp/myroles/supercool/tasks/main.yml 2>/dev/null",
+        ),
+        (
+            "ansible-lint",
+            ["lintable1", "lintable2", "lintable3"],
+            "ansible-lint lintable1 lintable2 lintable3 2>/dev/null",
+        ),
+        (
+            "ansible-lint",
+            ["lintable1", "lint\"able'2", "lint able3"],
+            "ansible-lint lintable1 'lint\"able'\"'\"'2' 'lint able3' 2>/dev/null",
+        ),
+    ],
+    ids=[
+        "only executable command",
+        "executable command with one argument",
+        "executable command with several arguments",
+        "executable command with arguments containing quotes and spaces",
+    ],
+)
+def test_command_base_runner_wrap_sh(orig_exec, orig_args, sh_c_arg):
+    """test CommandBaseRunner properly wraps commands with sh -c when asked."""
+
+    # default (no sh wrap)
+    cr = api.CommandRunner(orig_exec, cmdline=orig_args)
+    cr.generate_run_command_args()
+    assert cr._runner_args.get("executable_cmd") == orig_exec
+    assert cr._runner_args.get("cmdline_args") == orig_args
+
+    # explicit sh wrap
+    cr = api.CommandRunner(orig_exec, cmdline=orig_args, wrap_sh=True)
+    cr.generate_run_command_args()
+    assert cr._runner_args.get("executable_cmd") == "/bin/sh"
+    assert cr._runner_args.get("cmdline_args") == ["-c", sh_c_arg]
