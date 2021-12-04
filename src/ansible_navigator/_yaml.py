98548101b6e57f3fb5b194da45e76e43e3541c18
Bradley A. Thornton <bthornto@redhat.com>
2021-12-03 18:09:00 -0800

    Clarify some type ignores (#639)
    [typing] Clarify some type ignores, remove uneeded
    
    Fixes #638
    Specify the reason for some # type: ignore statements, remove ignore statements that are needed
    
    Reviewed-by: Sviatoslav Sydorenko <webknjaz+github/profile@redhat.com>
    Reviewed-by: Bradley A. Thornton <bthornto@redhat.com>
    Reviewed-by: None <None>

--- src/ansible_navigator/_yaml.py
+++ src/ansible_navigator/_yaml.py
@@ -11,14 +11,14 @@ import yaml  # noqa: F401
 try:
     from yaml import CDumper as Dumper
 except ImportError:
-    from yaml import Dumper  # type: ignore # noqa: F401
+    from yaml import Dumper  # type: ignore[misc] # noqa: F401
 
 try:
     from yaml import CLoader as Loader
     from yaml import CSafeLoader as SafeLoader
 except ImportError:
-    from yaml import Loader  # type: ignore # noqa: F401
-    from yaml import SafeLoader  # type: ignore # noqa: F401
+    from yaml import Loader  # type: ignore[misc] # noqa: F401
+    from yaml import SafeLoader  # type: ignore[misc] # noqa: F401
 
 
 class YamlStyle(NamedTuple):
