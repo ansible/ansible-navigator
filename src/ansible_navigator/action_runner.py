bf5b0069f5a984f029b675e936921dc74a71012f
cidrblock <bthornto@redhat.com>
2021-12-08 11:47:23 -0800

    restore previous pylint fix

--- src/ansible_navigator/action_runner.py
+++ src/ansible_navigator/action_runner.py
@@ -14,7 +14,7 @@ from .ui_framework import UIConfig
 from .ui_framework import UserInterface
 
 if TYPE_CHECKING:
-    from _curses import _CursesWindow  # pylint: disable= no-name-in-module
+    from _curses import _CursesWindow  # pylint: disable=no-name-in-module
 
     Window = _CursesWindow
 else:
