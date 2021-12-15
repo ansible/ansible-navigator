f36dd17f9d0b925cb5d41af468c7fb2e9a4a6a97
cidrblock <bthornto@redhat.com>
2021-12-15 09:32:47 -0800

    member-iterable

--- src/ansible_navigator/ui_framework/form.py
+++ src/ansible_navigator/ui_framework/form.py
@@ -28,28 +28,36 @@ class Form:
     def present(self, screen, ui_config):
         """present the form the to user and return the results"""
         if self.type is FormType.FORM:
+            # pylint: disable=no-member
             self.fields.append(
                 FieldButton(
                     name="submit", text="Submit", validator=FormValidators.all_true, color=10
                 )
             )
             self.fields.append(FieldButton(name="cancel", text="Cancel", color=9))
+            # pylint: enable=no-member
         elif self.type is FormType.NOTIFICATION:
+            # pylint: disable=no-member
             self.fields.append(
                 FieldButton(
                     name="submit", text=" Ok ", validator=FormValidators.no_validation, color=10
                 )
             )
+            # pylint: enable=no-member
         elif self.type is FormType.WORKING:
             pass
 
         FormPresenter(form=self, screen=screen, ui_config=ui_config).present()
         try:
+            # pylint: disable=not-an-iterable
             self.submitted = next(field for field in self.fields if field.name == "submit").pressed
+            # pylint: enable=not-an-iterable
         except StopIteration:
             self.submitted = False
         try:
+            # pylint: disable=not-an-iterable
             self.cancelled = next(field for field in self.fields if field.name == "cancel").pressed
+            # pylint: enable=not-an-iterable
         except StopIteration:
             self.cancelled = False
         return self
