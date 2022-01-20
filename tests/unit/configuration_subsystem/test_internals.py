fea5ec5907e5a96940ae3c89f296166e78908382
cidrblock <bthornto@redhat.com>
2022-01-20 02:37:05 -0800

    some linting changes

--- tests/unit/configuration_subsystem/test_internals.py
+++ tests/unit/configuration_subsystem/test_internals.py
@@ -1,9 +1,8 @@
-"""Test the internals of a NavigatorConfiguration"""
+"""Test the internals of a NavigatorConfiguration."""
 
 import os
 
 from copy import deepcopy
-
 
 from ansible_navigator.configuration_subsystem import Constants
 from ansible_navigator.configuration_subsystem import NavigatorConfiguration
@@ -15,13 +14,17 @@ from .defaults import TEST_FIXTURE_DIR
 def test_settings_file_path_file_none():
     """Confirm a settings file path is not stored in the internals when not present."""
     args = deepcopy(NavigatorConfiguration)
-    _messages, _exit_messages = parse_and_update(params=[], args=args, initial=True)
+    parse_and_update(params=[], args=args, initial=True)
     assert args.internals.settings_file_path is None
     assert args.internals.settings_source == Constants.NONE
 
 
 def test_settings_file_path_file_system(monkeypatch):
-    """Confirm a settings file path is stored in the internals when searched."""
+    """Confirm a settings file path is stored in the internals when searched.
+
+    :param monkeypatch: Fixture providing these helper methods for safely patching and mocking
+        functionality in tests
+    """
     settings_file = "ansible-navigator.yml"
     settings_file_path = os.path.join(TEST_FIXTURE_DIR, settings_file)
     args = deepcopy(NavigatorConfiguration)
@@ -31,17 +34,21 @@ def test_settings_file_path_file_system(monkeypatch):
 
     monkeypatch.setattr(os, "getcwd", getcwd)
 
-    _messages, _exit_messages = parse_and_update(params=[], args=args, initial=True)
+    parse_and_update(params=[], args=args, initial=True)
     assert args.internals.settings_file_path == settings_file_path
     assert args.internals.settings_source == Constants.SEARCH_PATH
 
 
 def test_settings_file_path_environment_variable(monkeypatch):
-    """Confirm a settings file path is stored in the internals when set via environment variable."""
+    """Confirm a settings file path is stored in the internals when set via environment variable.
+
+    :param monkeypatch: Fixture providing these helper methods for safely patching and mocking
+        functionality in tests
+    """
     settings_file = "ansible-navigator.yml"
     settings_file_path = os.path.join(TEST_FIXTURE_DIR, settings_file)
     monkeypatch.setenv("ANSIBLE_NAVIGATOR_CONFIG", settings_file_path)
     args = deepcopy(NavigatorConfiguration)
-    _messages, _exit_messages = parse_and_update(params=[], args=args, initial=True)
+    parse_and_update(params=[], args=args, initial=True)
     assert args.internals.settings_file_path == settings_file_path
     assert args.internals.settings_source == Constants.ENVIRONMENT_VARIABLE
