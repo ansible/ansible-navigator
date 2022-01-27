c0220cc4bd54a76fc4a99736e25d6b69f3c19583
cidrblock <bthornto@redhat.com>
2022-01-27 04:09:29 -0800

    notification fix

--- src/ansible_navigator/actions/sample_notification.py
+++ src/ansible_navigator/actions/sample_notification.py
@@ -36,7 +36,7 @@ class Action(App):
     KEGEX = r"^sample_notification$"
 
     def __init__(self, args: ApplicationConfiguration):
-        """Initialize the ``:sample_form`` action.
+        """Initialize the ``:sample_notification`` action.
 
         :param args: The current settings for the application
         """
