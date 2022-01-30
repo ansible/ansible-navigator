9832ff5f83a76fe07263ca9c529d70d10c78b00e
Bradley A. Thornton <bthornto@redhat.com>
2022-01-30 10:05:07 -0800

    Update share/ansible_navigator/utils/__init__.py
    Co-authored-by: Sviatoslav Sydorenko <wk.cvs.github@sydorenko.org.ua>

--- share/ansible_navigator/utils/__init__.py
+++ share/ansible_navigator/utils/__init__.py
@@ -1,4 +1,4 @@
-"""Utility scripts that will be mounted into the execution environment.
+"""Utility scripts mounted into the execution environment.
 
 The purpose of these files is to introspect a running execution environment
 and provide information back to the application.
