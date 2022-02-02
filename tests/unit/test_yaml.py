2a1277ba4619c3835be8c14578aefdda408d04a5
cidrblock <bthornto@redhat.com>
2022-02-02 14:20:40 -0800

    Add tests

--- tests/unit/test_yaml.py
+++ tests/unit/test_yaml.py
@@ -1,6 +1,10 @@
 """Tests related to the yaml abstraction layer.
 """
 
+from typing import Any
+from unittest.mock import patch
+
+from yaml import Dumper
 import ansible_navigator._yaml as yaml_import
 
 
@@ -9,3 +13,22 @@ def test_check_yaml_imports():
     assert yaml_import.yaml is not None
     assert yaml_import.Dumper is not None
     assert yaml_import.Loader is not None
+
+
+def test_no_anchor_alias():
+    """Ensure anchors and aliases are not present with ingore_aliases present."""
+    data = {"integer": 42}
+    many_data = [data, data, data]
+    result = yaml_import.human_dump(many_data)
+    assert "&id" not in result
+    assert "*id" not in result
+
+
+@patch("ansible_navigator._yaml.HumanDumper.ignore_aliases", Dumper.ignore_aliases)
+def test_anchor_alias():
+    """Ensure anchors and aliases are present with ignore_aliases defaulted."""
+    data = {"integer": 42}
+    many_data = [data, data, data]
+    result = yaml_import.human_dump(many_data)
+    assert "&id" in result
+    assert "*id" in result
