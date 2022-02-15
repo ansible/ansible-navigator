ba54f494bd32c2d4de030bc65e50916a3d9bb5ca
Bradley A. Thornton <bthornto@redhat.com>
2022-02-15 11:05:26 -0800

    Normalize stdout return from actions (#946)
    * Normalize stdout return

---
+++ src/ansible_navigator/action_defs.py
@@ -0,0 +1,25 @@
+"""Pre-defined objects used by actions."""
+
+
+from typing import NamedTuple
+from typing import Union
+
+
+class RunReturn(NamedTuple):
+    """The base return object for any action's run method."""
+
+    #: A message to display immediately before exiting
+    message: str
+    #: The return code for the for the process
+    return_code: int
+
+
+class RunInteractiveReturn(RunReturn):
+    """The return object for an action's run interactive method."""
+
+
+class RunStdoutReturn(RunReturn):
+    """The return object for an action's run stdout method."""
+
+
+ActionReturn = Union[RunReturn, RunInteractiveReturn, RunStdoutReturn]
