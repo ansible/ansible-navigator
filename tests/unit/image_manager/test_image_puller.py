420883f614ec6b007a2b7816d7ba76880d7c06b1
cidrblock <bthornto@redhat.com>
2022-02-15 07:15:02 -0800

    Undo pull_policy changes

--- tests/unit/image_manager/test_image_puller.py
+++ tests/unit/image_manager/test_image_puller.py
@@ -41,7 +41,7 @@ def test_do_have(valid_container_engine, data):
         container_engine=valid_container_engine,
         image=DEFAULT_CONTAINER_IMAGE,
         arguments=Constants.NOT_SET,
-        policy=data.pull_policy,
+        pull_policy=data.pull_policy,
     )
     image_puller.assess()
     assert image_puller.assessment.pull_required == data.pull_required
@@ -63,7 +63,7 @@ def test_do_have_but_latest(valid_container_engine, data):
         container_engine=valid_container_engine,
         image=SMALL_TEST_IMAGE,
         arguments=Constants.NOT_SET,
-        policy=data.pull_policy,
+        pull_policy=data.pull_policy,
     )
     image_puller.assess()
     assert image_puller.assessment.pull_required == data.pull_required
@@ -85,7 +85,7 @@ def test_missing_locally(valid_container_engine, data):
         container_engine=valid_container_engine,
         image=uuid_str,
         arguments=Constants.NOT_SET,
-        policy=data.pull_policy,
+        pull_policy=data.pull_policy,
     )
     image_puller.assess()
     assert image_puller.assessment.pull_required == data.pull_required
@@ -108,7 +108,7 @@ def test_will_have(valid_container_engine, pullable_image, data):
         container_engine=valid_container_engine,
         image=pullable_image,
         arguments=Constants.NOT_SET,
-        policy=data.pull_policy,
+        pull_policy=data.pull_policy,
     )
     image_puller.assess()
     assert image_puller.assessment.pull_required == data.pull_required
@@ -140,7 +140,7 @@ def test_tag_parsing(image, expected_tag):
         container_engine="podman",
         image=image,
         arguments=Constants.NOT_SET,
-        policy="tag",
+        pull_policy="tag",
     )
     image_puller._extract_tag()  # pylint: disable=protected-access
     assert image_puller._image_tag == expected_tag  # pylint: disable=protected-access
@@ -152,7 +152,7 @@ def test_pull_with_args():
         container_engine="podman",
         image="my_image",
         arguments=["--tls-verify=false"],
-        policy="tag",
+        pull_policy="tag",
     )
     result = image_puller._generate_pull_command()  # pylint: disable=protected-access
     expected = "podman pull --tls-verify=false my_image"
