0390a0509f925a8a286247dad32613fa062e3edc
cidrblock <bthornto@redhat.com>
2022-03-03 14:57:53 -0800

    Restore exit code

--- tests/integration/test_stdout_exit_codes.py
+++ tests/integration/test_stdout_exit_codes.py
@@ -1,12 +1,16 @@
 """check return codes from mode ``stdout``
 """
 import os
+import subprocess
 
+from pathlib import Path
+from typing import List
 from typing import NamedTuple
 from typing import Tuple
 
 import pytest
 
+from ansible_navigator.utils.functions import shlex_join
 from ..defaults import DEFAULT_CONTAINER_IMAGE
 from ..defaults import FIXTURES_DIR
 
@@ -116,3 +120,163 @@ def test(action_run_stdout, params, test_data):
         std_stream = stdout if params["execution_environment"] else stderr
         assert test_data.present in std_stream
         assert test_data.message in run_stdout_return.message
+
+
+class StdoutCliTest(NamedTuple):
+    """Definition of a stdout cli test."""
+
+    comment: str
+    """Description of the test"""
+    execution_environment: bool
+    """Run in an execution environment"""
+    params: List[str]
+    """Parameters for the subcommand"""
+    playbook_artifact_enable: bool
+    """Enable playbook artifact creation"""
+    return_code: int
+    """Expected return code"""
+    stderr_find: List
+    """Substring to search for in stderr"""
+    stdout_find: List
+    """Substring to search for in stdout"""
+    subcommand: str
+    """The name of the subcommand"""
+    mode: str = "stdout"
+    """The mode to run in"""
+
+    def __str__(self) -> str:
+        """Provide a test id."""
+        return self.comment
+
+    @property
+    def command(self) -> List[str]:
+        """Provide the constructed command"""
+        return ["ansible-navigator", self.subcommand] + self.params + ["--mode", self.mode]
+
+
+# Intentionally not using parametrize so the behavior can be documented
+StdoutCliTests = (
+    StdoutCliTest(
+        comment="run pass/ee/pae",
+        execution_environment=True,
+        subcommand="run",
+        params=[PLAYBOOK],
+        playbook_artifact_enable=True,
+        return_code=0,
+        stderr_find=[],
+        stdout_find=["ok=1"],
+    ),
+    StdoutCliTest(
+        comment="run pass/ee/no pae",
+        execution_environment=True,
+        subcommand="run",
+        params=[PLAYBOOK],
+        playbook_artifact_enable=False,
+        return_code=0,
+        stderr_find=[],
+        stdout_find=["ok=1"],
+    ),
+    StdoutCliTest(
+        comment="run pass/no ee/pae",
+        execution_environment=False,
+        subcommand="run",
+        params=[PLAYBOOK],
+        playbook_artifact_enable=True,
+        return_code=0,
+        stderr_find=[],
+        stdout_find=["ok=1"],
+    ),
+    StdoutCliTest(
+        comment="run pass/no ee/no pae",
+        execution_environment=False,
+        subcommand="run",
+        params=[PLAYBOOK],
+        playbook_artifact_enable=False,
+        return_code=0,
+        stderr_find=[],
+        stdout_find=["ok=1"],
+    ),
+    StdoutCliTest(
+        comment="run fail/ee/pae",
+        execution_environment=True,
+        subcommand="run",
+        params=["no_such_playbook.yaml"],
+        playbook_artifact_enable=True,
+        return_code=1,
+        stderr_find=["review the log"],  # navigator writes to stderr
+        stdout_find=["could not be found"],  # ee/pae only produces stdout
+    ),
+    StdoutCliTest(
+        comment="run fail/ee/no pae",
+        execution_environment=True,
+        subcommand="run",
+        params=["no_such_playbook.yaml"],
+        playbook_artifact_enable=False,
+        return_code=1,
+        stderr_find=["review the log"],  # navigator writes to stderr
+        stdout_find=["could not be found"],  # ee only produces stdout
+    ),
+    StdoutCliTest(
+        comment="run fail/no ee/pae",
+        execution_environment=False,
+        subcommand="run",
+        params=["no_such_playbook.yaml"],
+        playbook_artifact_enable=True,
+        return_code=1,
+        stderr_find=["review the log"],  # navigator writes to stderr
+        stdout_find=["could not be found"],  # pae produces stdout
+    ),
+    StdoutCliTest(
+        comment="run fail/no ee/no pae",
+        execution_environment=False,
+        subcommand="run",
+        params=["no_such_playbook.yaml"],
+        playbook_artifact_enable=False,
+        return_code=1,
+        stderr_find=["review the log", "could not be found"],  # ansible/navigator write to stderr
+        stdout_find=[],
+    ),
+)
+
+
+@pytest.mark.parametrize(argnames="data", argvalues=StdoutCliTests, ids=str)
+def test_run_through_cli(
+    tmp_path: Path,
+    data: StdoutCliTest,
+) -> None:
+    """Test for a return code from run through a shell.
+
+    :param data: The test data
+    :raises AssertionError: When no virtual environment found
+    """
+    venv_path = os.environ.get("VIRTUAL_ENV")
+    if venv_path is None:
+        raise AssertionError(
+            "VIRTUAL_ENV environment variable was not set but tox should have set it.",
+        )
+    venv = Path(venv_path, "bin", "activate")
+    log_file = str(Path(tmp_path, "log.txt"))
+    artifact_file = str(Path(tmp_path, "artifact.json"))
+
+    command = data.command + [
+        "--lf",
+        log_file,
+        "--pae",
+        str(data.playbook_artifact_enable),
+        "--pas",
+        artifact_file,
+        "--ee",
+        str(data.execution_environment),
+    ]
+    bash_wrapped = f"/bin/sh -c 'source {venv!s} && {shlex_join(command)}'"
+    proc_out = subprocess.run(
+        bash_wrapped,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        check=False,
+        universal_newlines=True,
+        shell=True,
+    )
+    assert all(find in proc_out.stdout for find in data.stdout_find)
+    assert all(find in proc_out.stderr for find in data.stderr_find)
+    assert data.return_code == proc_out.returncode
