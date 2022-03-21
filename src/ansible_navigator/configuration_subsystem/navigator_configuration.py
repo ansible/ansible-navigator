2623717b64f53eb497b255d7f160285e22b6b9c6
Bradley A. Thornton <bthornto@redhat.com>
2022-03-21 07:34:21 -0700

    Switch to `ANSIBLE_INVENTORY` for inventory environment variable (#1113)
    * Switch to ansible_inventory
    
    * Cahngelog

--- src/ansible_navigator/configuration_subsystem/navigator_configuration.py
+++ src/ansible_navigator/configuration_subsystem/navigator_configuration.py
@@ -365,7 +365,7 @@ NavigatorConfiguration = ApplicationConfiguration(
         SettingsEntry(
             name="inventory",
             cli_parameters=CliParameters(action="append", nargs="+", short="-i"),
-            environment_variable_override="ansible_navigator_inventories",
+            environment_variable_override="ansible_inventory",
             settings_file_path_override="ansible.inventory.paths",
             short_description="Specify an inventory file path or comma separated host list",
             subcommands=["inventory", "run"],
