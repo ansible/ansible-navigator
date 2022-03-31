d7a2a2a8196078023789ccf667e47f1ad387a792
Rick Elrod <rick@elrod.me>
2022-03-31 14:31:12 -0500

    initial go at lint tests
    Signed-off-by: Rick Elrod <rick@elrod.me>

---
+++ tests/integration/actions/lint/base.py
@@ -0,0 +1,85 @@
+"""Base class for lint interactive tests.
+"""
+import difflib
+import os
+
+import pytest
+
+from ....defaults import FIXTURES_DIR
+from ..._common import retrieve_fixture_for_step
+from ..._common import update_fixtures
+from ..._interactions import SearchFor
+from ..._interactions import UiTestStep
+from ..._tmux_session import TmuxSession
+
+
+LINT_FIXTURES = os.path.join(
+    FIXTURES_DIR,
+    "integration",
+    "actions",
+    "lint",
+)
+
+
+class BaseClass:
+    """Base class for interactive images tests."""
+
+    UPDATE_FIXTURES = False
+
+    @staticmethod
+    @pytest.fixture(scope="module", name="tmux_session")
+    def fixture_tmux_session(request):
+        """tmux fixture for this module"""
+        params = {
+            "unique_test_id": request.node.nodeid,
+        }
+        with TmuxSession(**params) as tmux_session:
+            yield tmux_session
+
+    def test(self, request, tmux_session, step):
+        """Run the tests for lint, mode and ``ee`` set in child class."""
+
+        if step.search_within_response is SearchFor.HELP:
+            search_within_response = ":help help"
+        elif step.search_within_response is SearchFor.PROMPT:
+            search_within_response = tmux_session.cli_prompt
+        elif step.search_within_response is SearchFor.WARNING:
+            search_within_response = "WARNING"
+        else:
+            search_within_response = step.search_within_response
+
+        received_output = tmux_session.interaction(
+            value=step.user_input,
+            search_within_response=search_within_response,
+        )
+
+        fixtures_update_requested = (
+            self.UPDATE_FIXTURES
+            or os.environ.get("ANSIBLE_NAVIGATOR_UPDATE_TEST_FIXTURES") == "true"
+        )
+        if fixtures_update_requested:
+            update_fixtures(
+                request,
+                step.step_index,
+                received_output,
+                step.comment,
+                additional_information={
+                    "present": step.present,
+                    "absent": step.absent,
+                    "compared_fixture": not any((step.present, step.absent)),
+                },
+            )
+
+        page = " ".join(received_output)
+
+        if step.present:
+            assert all(present in page for present in step.present)
+
+        if step.absent:
+            assert not any(absent in page for absent in step.absent)
+
+        if not any((step.present, step.absent)):
+            expected_output = retrieve_fixture_for_step(request, step.step_index)
+            assert expected_output == received_output, "\n" + "\n".join(
+                difflib.unified_diff(expected_output, received_output, "expected", "received"),
+            )
---
+++ tests/integration/actions/lint/test_direct_interactive_no_errors_ee.py
@@ -0,0 +1,32 @@
+"""Tests for lint (with no errors) from CLI, interactive, with an EE.
+"""
+import os
+import pytest
+
+from ..._interactions import Command
+from ..._interactions import UiTestStep
+from ..._interactions import add_indices
+from ..._interactions import step_id
+from .base import LINT_FIXTURES
+from .base import BaseClass
+
+
+CLI = Command(subcommand="lint", cmdline=os.path.join(LINT_FIXTURES, "no_errors",), execution_environment=True).join()
+
+steps = (
+    UiTestStep(
+        user_input=CLI,
+        comment="ansible-navigator lint top window",
+        present=["no lint issues"],
+        search_within_response="SUCCESS",
+    ),
+)
+
+steps = add_indices(steps)
+
+
+@pytest.mark.parametrize("step", steps, ids=step_id)
+class Test(BaseClass):
+    """Run the tests for images from CLI, interactive, with an EE."""
+
+    UPDATE_FIXTURES = False
