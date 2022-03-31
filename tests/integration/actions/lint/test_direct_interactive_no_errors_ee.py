77d087623381042334a8db8ba6a1fbba7a4266f0
Rick Elrod <rick@elrod.me>
2022-03-31 16:08:21 -0500

    initial go at lint tests (#1170)
    Signed-off-by: Rick Elrod <rick@elrod.me>

---
+++ tests/integration/actions/lint/test_direct_interactive_no_errors_ee.py
@@ -0,0 +1,40 @@
+"""Tests for lint (with no errors) from CLI, interactive, with an EE.
+"""
+import os
+
+import pytest
+
+from ..._interactions import Command
+from ..._interactions import UiTestStep
+from ..._interactions import add_indices
+from ..._interactions import step_id
+from .base import LINT_FIXTURES
+from .base import BaseClass
+
+
+CLI = Command(
+    subcommand="lint",
+    cmdline=os.path.join(
+        LINT_FIXTURES,
+        "no_errors",
+    ),
+    execution_environment=True,
+).join()
+
+steps = (
+    UiTestStep(
+        user_input=CLI,
+        comment="ansible-navigator lint top window",
+        present=["no lint issues"],
+        search_within_response="Success",
+    ),
+)
+
+steps = add_indices(steps)
+
+
+@pytest.mark.parametrize("step", steps, ids=step_id)
+class Test(BaseClass):
+    """Run the tests for lint from CLI, interactive, with an EE."""
+
+    UPDATE_FIXTURES = False
---
+++ tests/integration/actions/lint/test_stdout_tmux.py
@@ -0,0 +1,48 @@
+"""Tests for ``lint`` from CLI, stdout."""
+import os
+
+import pytest
+
+from ..._interactions import Command
+from ..._interactions import SearchFor
+from ..._interactions import UiTestStep
+from ..._interactions import add_indices
+from .base import LINT_FIXTURES
+from .base import BaseClass
+
+
+stdout_tests = (
+    UiTestStep(
+        comment="lint stdout with errors",
+        user_input=Command(
+            subcommand="lint",
+            cmdline=LINT_FIXTURES,
+            precommand=f"cd {os.path.join(LINT_FIXTURES, '..')} ; ",
+            mode="stdout",
+            execution_environment=True,
+            preclear=True,
+        ).join(),
+        present=["Commands should not change things"],
+        search_within_response=SearchFor.PROMPT,
+    ),
+    UiTestStep(
+        comment="lint stdout with no errors",
+        user_input=Command(
+            subcommand="lint",
+            cmdline=os.path.join(LINT_FIXTURES, "no_errors"),
+            mode="stdout",
+            execution_environment=True,
+        ).join(),
+        absent=[".yml:"],
+        search_within_response=SearchFor.PROMPT,
+    ),
+)
+
+steps = add_indices(stdout_tests)
+
+
+@pytest.mark.parametrize("step", steps, ids=str)
+class Test(BaseClass):
+    """Run the tests for ``lint`` from CLI, mode stdout."""
+
+    UPDATE_FIXTURES = False
