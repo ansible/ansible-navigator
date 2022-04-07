b99095ec7e8d552ac3eb644abc29ce55b5a0cd6f
Bradley A. Thornton <bthornto@redhat.com>
2022-04-07 06:21:39 -0700

    Update creator-ee to 0.4.2 (#1190)
    * Creator ee to 0.4.2
    
    * Fixture updates

--- src/ansible_navigator/configuration_subsystem/navigator_configuration.py
+++ src/ansible_navigator/configuration_subsystem/navigator_configuration.py
@@ -363,7 +363,7 @@ NavigatorConfiguration = ApplicationConfiguration(
             cli_parameters=CliParameters(short="--eei"),
             settings_file_path_override="execution-environment.image",
             short_description="Specify the name of the execution environment image",
-            value=SettingsEntryValue(default="quay.io/ansible/creator-ee:v0.4.0"),
+            value=SettingsEntryValue(default="quay.io/ansible/creator-ee:v0.4.2"),
             version_added="v1.0",
         ),
         SettingsEntry(
