c67271a8a7a222e10d10a7a5fb3100b3b61b797b
Bradley A. Thornton <bthornto@redhat.com>
2022-12-14 09:28:50 -0800

    Source runtime and test image from dockerfile (#1358)
    * Source runtime and test iamge from dockerfile
    
    * Leave quay for now
    
    * Use files not open_test
    
    * Fix dependabot path

---
+++ src/ansible_navigator/utils/packaged_data.py
@@ -0,0 +1,19 @@
+"""Functionality related to the retrieval of packaged data files."""
+
+from .compatibility import importlib_resources
+
+
+def retrieve_content(app_name: str, filename: str) -> str:
+    """Retrieve the content of a packaged data file.
+
+    :param app_name: The name of the application.
+    :param filename: The name of the file to retrieve.
+    :returns: The content of the file.
+    """
+    data_directory = "data"
+    package = f"{app_name}.{data_directory}"
+
+    with importlib_resources.files(package).joinpath(filename).open("r", encoding="utf-8") as fh:
+        content = fh.read()
+
+    return content
