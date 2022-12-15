30f030bfd3f50d4d86c00352604c77f0e9a63b6f
Bradley A. Thornton <bthornto@redhat.com>
2022-12-15 09:31:34 -0800

    Use version for filter (#1367)

--- tests/integration/actions/images/test_direct_interactive_noee.py
+++ tests/integration/actions/images/test_direct_interactive_noee.py
@@ -5,7 +5,7 @@ from ..._interactions import Command
 from ..._interactions import UiTestStep
 from ..._interactions import add_indices
 from ..._interactions import step_id
-from .base import IMAGE_SHORT
+from .base import IMAGE_VERSION
 from .base import BaseClass
 from .base import base_steps
 
@@ -17,7 +17,7 @@ initial_steps = (
     UiTestStep(
         user_input=CLI,
         comment="ansible-navigator images top window",
-        present=[IMAGE_SHORT],
+        present=[IMAGE_VERSION],
     ),
 )
 
