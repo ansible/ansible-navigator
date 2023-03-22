30cab4501cd49c22fe64a4bcd7766312aae17c21
Bradley A. Thornton <bthornto@redhat.com>
2023-03-22 13:49:34 -0700

    Add tests

---
+++ tests/integration/actions/run/test_direct_interactive_ee_collection.py
@@ -0,0 +1,35 @@
+"""Tests for run from CLI, interactive, with an EE."""
+import pytest
+
+from ..._interactions import Command
+from ..._interactions import UiTestStep
+from ..._interactions import add_indices
+from ..._interactions import step_id_padded
+from .base import BaseClass
+from .base import base_steps
+from .base import common_fixture_dir
+from .base import playbook_collection
+
+
+cmdline = (
+    f"{playbook_collection} --eev {common_fixture_dir}:{common_fixture_dir}"
+    f" --senv ANSIBLE_COLLECTIONS_PATHS={common_fixture_dir}"
+)
+CLI = Command(subcommand="run", cmdline=cmdline, execution_environment=True).join()
+
+initial_steps = (
+    UiTestStep(
+        user_input=CLI,
+        comment="ansible-navigator run playbook",
+        search_within_response=["Complete", "Successful"],
+    ),
+)
+
+steps = add_indices(initial_steps + base_steps)
+
+
+@pytest.mark.parametrize("step", steps, ids=step_id_padded)
+class Test(BaseClass):
+    """Run the tests for run from CLI, interactive, with an EE."""
+
+    UPDATE_FIXTURES = False
