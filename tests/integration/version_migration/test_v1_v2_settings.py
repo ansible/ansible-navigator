92ff5f08fe30c8670b5a0a0287ea2bf6c09dbf99
Bradley A. Thornton <bthornto@redhat.com>
2023-03-31 12:31:09 -0700

    Update schema (#1472)
    * Update schema
    
    * Version 3 fix for test
    
    * Update test

--- tests/integration/version_migration/test_v1_v2_settings.py
+++ tests/integration/version_migration/test_v1_v2_settings.py
@@ -68,7 +68,7 @@ def test_all(
         shutil.copy(destination, corrected)
 
     assert any(
-        "ansible-navigator 2." in line for line in result
+        "ansible-navigator 3." in line for line in result
     ), "(Note: requires recent tags, `git fetch --all`)"
     assert filecmp.cmp(destination, corrected)
     assert filecmp.cmp(source, backup)
