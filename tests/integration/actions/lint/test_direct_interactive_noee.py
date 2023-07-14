5a4de7fe99d5c380efd5beaa63eb86276250bfeb
shatakshiiii <shatakshimishra01@gmail.com>
2023-07-14 17:07:09 +0530

    test_direct_interactive_noee tests

---
+++ tests/integration/actions/lint/test_direct_interactive_noee.py
@@ -0,0 +1,30 @@
+"""Tests for lint from CLI, interactive, without an EE."""
+import pytest
+
+from tests.integration._interactions import Command
+from tests.integration._interactions import UiTestStep
+from tests.integration._interactions import add_indices
+from tests.integration._interactions import step_id
+
+from .base import LINT_FIXTURES
+from .base import BaseClass
+
+
+CLI = Command(subcommand="lint", cmdline=LINT_FIXTURES, execution_environment=False).join()
+
+steps: tuple[UiTestStep, ...] = (
+    UiTestStep(
+        user_input=CLI,
+        comment="lint interactive without ee",
+        present=["Unable to find a working copy"],
+    ),
+)
+
+steps = add_indices(steps)
+
+
+@pytest.mark.parametrize("step", steps, ids=step_id)
+class Test(BaseClass):
+    """Run the tests for lint from CLI, interactive, without an EE."""
+
+    UPDATE_FIXTURES = False
