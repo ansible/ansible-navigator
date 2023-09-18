50e984f7d7affdc608289e7e99b0bb4c74c2517c
Shatakshi Mishra <shatakshimishra01@gmail.com>
2023-09-18 17:58:34 +0530

    Add tests for `lint`, without an EE (#1574)
    * Tests for lint in mode stdout without ee
    
    * test_direct_interactive_noee tests
    
    * change fixture
    
    * fix test_direct_interactive_noee
    
    * fix test_stdout_tmux
    
    * add tests for welcome interactive noee
    
    * fix welcome screen output
    
    * fix typo in collection's docstring
    
    * Change the stdout mode noee test, after adding lint as a requirement
    
    * Modify interactive test
    
    * Revert the changes for stdout_tmux test

---
+++ tests/integration/actions/lint/test_welcome_interactive_noee.py
@@ -0,0 +1,41 @@
+"""Tests for lint from welcome, interactive, without an EE."""
+import pytest
+
+from tests.integration._interactions import Command
+from tests.integration._interactions import UiTestStep
+from tests.integration._interactions import add_indices
+from tests.integration._interactions import step_id
+
+from .base import LINT_FIXTURES
+from .base import BaseClass
+
+
+CLI = Command(execution_environment=False).join()
+
+initial_steps = (
+    UiTestStep(user_input=CLI, comment="welcome screen"),
+    UiTestStep(
+        user_input=f":lint {LINT_FIXTURES}",
+        comment="ansible-navigator lint results list",
+        present=["All tasks should be named"],
+    ),
+    UiTestStep(
+        user_input=":2",
+        comment="lint result content page",
+        present=["issue_path:"],
+    ),
+    UiTestStep(
+        user_input=":back",
+        comment="ansible-navigator lint top window",
+        present=["All tasks should be named"],
+    ),
+)
+
+steps = add_indices(initial_steps)
+
+
+@pytest.mark.parametrize("step", steps, ids=step_id)
+class Test(BaseClass):
+    """Run the tests for lint from welcome, interactive, without an EE."""
+
+    UPDATE_FIXTURES = False
