0b32edd7967277d847d03f607115986f06302181
shatakshiiii <shatakshimishra01@gmail.com>
2023-09-21 19:08:54 +0530

    Add tests

--- tests/unit/image_manager/test_image_puller.py
+++ tests/unit/image_manager/test_image_puller.py
@@ -1,6 +1,8 @@
 """Unit tests for image puller."""
 
+import os
 import shlex
+import subprocess
 import uuid
 
 from typing import NamedTuple
@@ -194,3 +196,20 @@ def test_pull_with_args():
     assert result == expected_string
     expected_list = ["podman", "pull", "--tls-verify", "false", "my_image"]
     assert shlex.split(result) == expected_list
+
+
+def test_pull_with_env_arg():
+    """Ensure command is generated with additional arguments."""
+    image_puller = ImagePuller(
+        container_engine="podman",
+        image="my_image",
+        arguments=["--authfile", "${XDG_RUNTIME_DIR}/containers/auth.json"],
+        pull_policy="tag",
+    )
+    result = image_puller._generate_pull_command()  # pylint: disable=protected-access
+    cmd_to_run = f"echo {result}"
+    proc = subprocess.run(
+        cmd_to_run, check=True, shell=True, env=os.environ, capture_output=True, text=True
+    )
+    assert "XDG_RUNTIME_DIR" not in proc.stdout
+    assert "containers/auth.json" in proc.stdout
