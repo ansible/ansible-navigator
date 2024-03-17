5b32e632a4d2df9e34203c9f3c7691fdeca04c36
Tony Narlock <tony@git-pull.com>
2024-03-17 09:47:51 -0500

    TmuxSession: Full window splits

--- tests/integration/_tmux_session.py
+++ tests/integration/_tmux_session.py
@@ -122,9 +122,16 @@ class TmuxSession:
         self._window = self._session.new_window(self._session_name, attach=False)
         self._window.resize(height=self._pane_height * 2, width=self._pane_width * 2)
         self._pane = self._window.panes[0]
-        self._pane.split(attach=False)
+        self._pane.split(
+            attach=False,
+            full_window_split=True,
+        )
         # split vertical
-        self._pane.split(direction=PaneDirection.Right, attach=False)
+        self._pane.split(
+            direction=PaneDirection.Right,
+            attach=False,
+            full_window_split=True,
+        )
         # attached to upper left
         self._pane.resize(height=self._pane_height, width=self._pane_width)
 
