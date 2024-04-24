6d39ec1b8ee019ddaf0810f91c1bc26012400d32
shatakshiiii <shatakshimishra01@gmail.com>
2024-04-24 11:22:21 +0530

    Fix test_catalog_collections.py

--- tests/unit/test_catalog_collections.py
+++ tests/unit/test_catalog_collections.py
@@ -1,54 +1,32 @@
 """Unit tests for catalog collections."""
+
+import multiprocessing
+
 from pathlib import Path
+from unittest.mock import patch
 
-import pytest
-import yaml
-from ansible.errors import AnsibleError
-from ansible.plugins.loader import fragment_loader
-from ansible.utils.plugin_docs import get_docstring
-
-from ansible_navigator.cli import cache_scripts
-from ansible_navigator.data.catalog_collections import get_doc_withast
-
-# module_content = '''
-# DOCUMENTATION = r"""
-# ---
-# module: vcenter_mod
-# short_description: Gather info vCenter extensions
-# description:
-# - This module can be used to gather information about vCenter extension.
-# author:
-# - test
-# extends_documentation_fragment:
-# - community.vmware.vmware.documentation
-# """
-
-# EXAMPLES = "Example usage here."
-# RETURN = "This function returns a value."
-# METADATA = "Author: John Doe"
-# '''
-
-plugin_path = Path("tests/fixtures/common/example.py")
-collection_name = "microsoft.ad"
+from ansible_navigator.data.catalog_collections import worker
 
 
-def test_catalog_collections() -> None:
-    """Import the catalog collections script."""
-    cache_scripts()
-    with pytest.raises(AnsibleError):
-        get_docstring(
-            filename=str(plugin_path),
-            fragment_loader=fragment_loader,
-            collection_name=collection_name,
-        )
+@patch("ansible_navigator.data.catalog_collections.get_docstring")
+def test_worker_with_failed_get_docstring(mock_get_docstring):
+    """Test the worker function when get_docstring fails."""
+    # Create the queues
+    pending_queue = multiprocessing.Queue()
+    completed_queue = multiprocessing.Queue()
 
-    with plugin_path.open(mode="r", encoding="utf-8") as f:
-        content = f.read()
-    doc, examples, returndocs, metadata = get_doc_withast(content)
-    doc, examples, returndocs, metadata = (
-        yaml.load(value, Loader=yaml.SafeLoader)
-        for value in (doc, examples, returndocs, metadata)
-    )
-    assert examples == "Example usage here."
-    assert returndocs == "This function returns a value."
-    assert metadata == {'Author': 'John Doe'}
+    plugin_path = Path("tests/fixtures/common/example.py")
+    collection_name = "microsoft.ad"
+    checksum = "12345"
+
+    # Add an entry to the pending queue
+    entry = collection_name, checksum, plugin_path
+    pending_queue.put(entry)
+
+    # Add a None entry to signal the end of processing
+    pending_queue.put(None)
+
+    worker(pending_queue, completed_queue)
+
+    plugin_path, data = completed_queue.get()
+    assert "vCenter" in data[1]
