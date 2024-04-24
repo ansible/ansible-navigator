25b3752e6cdb4c8ccd4e607b14b477e78c14dea7
shatakshiiii <shatakshimishra01@gmail.com>
2024-04-24 12:05:51 +0530

    More fixes in test_catalog_collections.py

--- tests/fixtures/common/module_1.py
+++ tests/fixtures/common/module_1.py
@@ -1,4 +1,4 @@
-"""Unit tests for catalog collections."""
+"""An ansible test module."""
 
 DOCUMENTATION = r"""
 ---
--- tests/unit/test_catalog_collections.py
+++ tests/unit/test_catalog_collections.py
@@ -3,19 +3,18 @@
 import multiprocessing
 
 from pathlib import Path
-from unittest.mock import patch
+from typing import Any
 
 from ansible_navigator.data.catalog_collections import worker
 
 
-@patch("ansible_navigator.data.catalog_collections.get_docstring")
-def test_worker_with_failed_get_docstring(mock_get_docstring):
+def test_worker_with_failed_get_docstring() -> None:
     """Test the worker function when get_docstring fails."""
     # Create the queues
-    pending_queue = multiprocessing.Queue()
-    completed_queue = multiprocessing.Queue()
+    pending_queue: multiprocessing.Queue[Any] = multiprocessing.Queue()
+    completed_queue: multiprocessing.Queue[Any] = multiprocessing.Queue()
 
-    plugin_path = Path("tests/fixtures/common/example.py")
+    plugin_path = Path("tests/fixtures/common/module_1.py")
     collection_name = "microsoft.ad"
     checksum = "12345"
 
