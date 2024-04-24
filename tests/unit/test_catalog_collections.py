9aba0e91c9d83d6dbb9da888c3fa7269b68a79a5
shatakshiiii <shatakshimishra01@gmail.com>
2024-04-24 14:35:50 +0530

    Add test_worker_with_mocked_get_docstring

--- tests/unit/test_catalog_collections.py
+++ tests/unit/test_catalog_collections.py
@@ -29,3 +29,46 @@ def test_worker_with_failed_get_docstring() -> None:
 
     plugin_path, data = completed_queue.get()
     assert "vCenter" in data[1]
+
+
+def test_worker_with_mocked_get_docstring() -> None:
+    """Test the worker function with mocked get_docstring."""
+    # def mock_get_docstring(*args, **kwargs):
+    #     return (
+    #         "Mocked docstring",
+    #         "Mocked examples",
+    #         "Mocked returndocs",
+    #         "Mocked metadata",
+    #     )
+
+    # def mock_get_docstring(*args, **kwargs):
+    #     raise Exception("Mocked exception")
+
+    # monkeypatch.setattr(
+    #     "ansible_navigator.data.catalog_collections.get_docstring",
+    #     mock_get_docstring,
+    # )
+
+    pending_queue: multiprocessing.Queue[Any] = multiprocessing.Queue()
+    completed_queue: multiprocessing.Queue[Any] = multiprocessing.Queue()
+
+    plugin_path = Path("tests/fixtures/common/xyz.py")
+    collection_name = "microsoft.ad"
+    checksum = "12345"
+
+    # Add an entry to the pending queue
+    entry = collection_name, checksum, plugin_path
+    pending_queue.put(entry)
+
+    # Add a None entry to signal the end of processing
+    pending_queue.put(None)
+
+    worker(pending_queue, completed_queue)
+
+    plugin_path, result = completed_queue.get()
+    # assert "Mocked docstring" in data[1]
+    assert plugin_path == "error"
+    assert "FileNotFoundError (get_docstring)" in result[2]
+
+    # Check if the error message is put into the completed_queue
+    # assert completed_queue.get() == ("error", ("12345", "tests/fixtures/common/xyz.py", "Exception (get_docstring): Mocked exception"))
