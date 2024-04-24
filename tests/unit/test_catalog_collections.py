13968910d7703f29c8f0b2d3acfaf5abf63c5782
shatakshiiii <shatakshimishra01@gmail.com>
2024-04-24 15:14:53 +0530

    More fixes in test

--- tests/unit/test_catalog_collections.py
+++ tests/unit/test_catalog_collections.py
@@ -9,7 +9,7 @@ from ansible_navigator.data.catalog_collections import worker
 
 
 def test_worker_with_failed_get_docstring() -> None:
-    """Test the worker function when get_docstring fails."""
+    """Test worker function when get_docstring fails and get_doc_withast method is used to parse the content."""
     # Create the queues
     pending_queue: multiprocessing.Queue[Any] = multiprocessing.Queue()
     completed_queue: multiprocessing.Queue[Any] = multiprocessing.Queue()
@@ -31,24 +31,8 @@ def test_worker_with_failed_get_docstring() -> None:
     assert "vCenter" in data[1]
 
 
-def test_worker_with_mocked_get_docstring() -> None:
-    """Test the worker function with mocked get_docstring."""
-    # def mock_get_docstring(*args, **kwargs):
-    #     return (
-    #         "Mocked docstring",
-    #         "Mocked examples",
-    #         "Mocked returndocs",
-    #         "Mocked metadata",
-    #     )
-
-    # def mock_get_docstring(*args, **kwargs):
-    #     raise Exception("Mocked exception")
-
-    # monkeypatch.setattr(
-    #     "ansible_navigator.data.catalog_collections.get_docstring",
-    #     mock_get_docstring,
-    # )
-
+def test_worker_with_invalid_plugin_path() -> None:
+    """Test the worker function when get_docstring has invalid plugin_path."""
     pending_queue: multiprocessing.Queue[Any] = multiprocessing.Queue()
     completed_queue: multiprocessing.Queue[Any] = multiprocessing.Queue()
 
@@ -65,10 +49,6 @@ def test_worker_with_mocked_get_docstring() -> None:
 
     worker(pending_queue, completed_queue)
 
-    plugin_path, result = completed_queue.get()
-    # assert "Mocked docstring" in data[1]
+    plugin_path, data = completed_queue.get()
     assert plugin_path == "error"
-    assert "FileNotFoundError (get_docstring)" in result[2]
-
-    # Check if the error message is put into the completed_queue
-    # assert completed_queue.get() == ("error", ("12345", "tests/fixtures/common/xyz.py", "Exception (get_docstring): Mocked exception"))
+    assert "FileNotFoundError (get_docstring)" in data[2]
